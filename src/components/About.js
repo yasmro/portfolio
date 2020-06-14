1a1235e445076eb3d9cb30b68976b1ad0a7f8439
Yu Ohno <kantillo0683@gmail.com>
2020-06-14 18:16:00 +0900

    add animation

--- src/components/About.js
+++ src/components/About.js
@@ -1,73 +1,14 @@
 import React, { Component } from 'react';
 import { motion } from "framer-motion";
+
+import { state as skills } from './skills'
+
 import '../style.css';
 
 class About extends React.Component {
   constructor() {
     super();
-    this.state = {
-      skills: [
-        {
-          id: 1,
-          category: "Programming",
-          skills:[
-            {
-              name: "HTML5 / CSS3",
-              description: "Bootstrap 4 and Web design"
-            },
-            {
-              name: "JavaScript",
-              description: "Available for jQuery and native. Also, I'm learning ReactJS now."
-            },
-            {
-              name: "Java, R, Shell Script",
-              description: "Analyze student's activity data in programming lectures"
-            },
-            {
-              name: "C#, MySQL",
-              description: ""
-            }
-          ]
-          
-        },
-        {
-          id: 2,
-          category: "Design",
-          skills:[
-            {
-              name: "Graphical Design",
-              description: "PowerPoint, Image and Clockface on smartwatch"
-            },
-            {
-              name: "Web Design",
-              description: "Available with HTML, CSS and JavaScript"
-            },
-            {
-              name: "Adobe Illustrator / Photoshop",
-              description: "Novice level"
-            }
-          ]
-        },
-        {
-          id: 3,
-          category: "Others",
-          skills:[
-            {
-              name: "Japanese Calligraphy",
-              description: "Experienced since 2009 and took 20+ awards on Japanese calligraphy concours before."
-            },
-            {
-              name: "Git / Subversion",
-              description: "Experienced with group development"
-            },
-            {
-              name: "English",
-              description: "TOEIC 675 (took on April 2018)"
-            }
-          ]
-        }
-      ]
-    };
+    this.state = skills;
   }
 
   renderskills(skills) {
---
+++ src/components/skills.js
@@ -0,0 +1,63 @@
+export var state = {
+    skills: [
+      {
+        id: 1,
+        category: "Programming",
+        skills:[
+          {
+            name: "HTML5 / CSS3",
+            description: "Bootstrap 4 and Web design"
+          },
+          {
+            name: "JavaScript",
+            description: "Available for jQuery and native. Also, I'm learning ReactJS now."
+          },
+          {
+            name: "Java, R, Shell Script",
+            description: "Analyze student's activity data in programming lectures"
+          },
+          {
+            name: "C#, MySQL",
+            description: ""
+          }
+        ]
+        
+      },
+      {
+        id: 2,
+        category: "Design",
+        skills:[
+          {
+            name: "Graphical Design",
+            description: "PowerPoint, Image and Clockface on smartwatch"
+          },
+          {
+            name: "Web Design",
+            description: "Available with HTML, CSS and JavaScript"
+          },
+          {
+            name: "Adobe Illustrator / Photoshop",
+            description: "Novice level"
+          }
+        ]
+      },
+      {
+        id: 3,
+        category: "Others",
+        skills:[
+          {
+            name: "Japanese Calligraphy",
+            description: "Experienced since 2009 and took 20+ awards on Japanese calligraphy concours before."
+          },
+          {
+            name: "Git / Subversion",
+            description: "Experienced with group development"
+          },
+          {
+            name: "English",
+            description: "TOEIC 675 (took on April 2018)"
+          }
+        ]
+      }
+    ]
+  };
