fe28b17aa87a0cd224a5db54db1ed9d6892a23cc
Yu Ohno <kantillo0683@gmail.com>
2020-06-14 13:56:24 +0900

    modified

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class Home extends Component {
   render(){
     return(
-      <div className="vh-100 bg-dark" Style="margin-top: -56px;">
-        <div className="position-absolute pl-lg-5 container text-white" Style = "top: calc(50vh - 56px);">
+      <div className="bg-dark vh-100" Style="margin-top:-56px;">
+        <div className="position-absolute pl-lg-5 container text-white" Style = "top: calc(50vh - 56px);" id="topmessage">
           <div className = "mb-5">
             <h1>Yu Ohno's Portfolio</h1>
             <h2>Move Emotion</h2>
           </div>
-          <p>Hi, I'm Yu Ohno from Japan and post-support engineer (mainly) , calligrapher, artist and designer. </p>
+          <p>Hi, I'm Yu Ohno from Japan and post-support engineer (mainly), calligrapher, artist and designer. </p>
         </div>
       </div>
     )
