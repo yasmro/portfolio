b69f0a300faf1d1e513be74f091ce017c4707a2e
Yu Ohno <kantillo0683@gmail.com>
2020-06-14 13:56:47 +0900

    Updates

--- precache-manifest.ffca1f48ffffa81a04ac8e929d9da643.js
+++ precache-manifest.ffca1f48ffffa81a04ac8e929d9da643.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a85fca81852a2ed95b569fa3891ced8b",
+    "revision": "f065c266ed1dbf64fabec81bf1f03cb7",
     "url": "/portfolioreact2020/index.html"
   },
   {
-    "revision": "dd800ee096360759766f",
-    "url": "/portfolioreact2020/static/css/main.70340238.chunk.css"
+    "revision": "7d6df1aae3ae72f864f5",
+    "url": "/portfolioreact2020/static/css/main.b76a1ec8.chunk.css"
   },
   {
     "revision": "85c5d45190f236062ee8",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolioreact2020/static/js/2.affe800f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dd800ee096360759766f",
-    "url": "/portfolioreact2020/static/js/main.3b55bdf9.chunk.js"
+    "revision": "7d6df1aae3ae72f864f5",
+    "url": "/portfolioreact2020/static/js/main.f9f05667.chunk.js"
   },
   {
     "revision": "1ce690e3909c724fa28c",
